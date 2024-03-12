f7cc58c9be39dcebe254a5c5ef431308c170acf2
edwardfeng-db <jinghao.feng@databricks.com>
2024-03-12 16:04:10 +0100

    update

--- common/recursion_tracking.go
+++ common/recursion_tracking.go
@@ -52,5 +52,5 @@ func getRecursionTrackingContext(rp RecursiveResourceProvider) recursionTracking
 }
 
 func getNameForType(t reflect.Type) string {
-	return strings.TrimPrefix(t.String(), "*")
+	return t.PkgPath() + strings.TrimPrefix(t.Name(), "*")
 }
